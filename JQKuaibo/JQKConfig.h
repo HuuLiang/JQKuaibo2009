1801dd7b92070bfe99110f64ed1c9c2fb11d7211
Yu Xulu <tonyfish@qq.com>
2016-01-22 13:08:03 +0800

    Move channel No configuration to plist.

--- JQKuaibo/JQKConfig.h
+++ JQKuaibo/JQKConfig.h
@@ -9,7 +9,9 @@
 #ifndef JQKConfig_h
 #define JQKConfig_h
 
-#define JQK_CHANNEL_NO           @"QB_VIDEO_IOS_B_00000001"
+#import "JQKConfiguration.h"
+
+#define JQK_CHANNEL_NO           [JQKConfiguration sharedConfig].channelNo
 #define JQK_REST_APP_ID          @"QUBA_2004"
 #define JQK_REST_PV              @100
 #define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Jiangxi Electronic Group Corporation Ltd."
