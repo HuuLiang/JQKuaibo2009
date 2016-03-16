5afeeead5a4d6e2864c4652ea448226a98e4a7ca
Yu Xulu <tonyfish@qq.com>
2016-03-16 21:39:24 +0800

    Certificate in config.

--- JQKuaibo/JQKConfig.h
+++ JQKuaibo/JQKConfig.h
@@ -14,7 +14,7 @@
 #define JQK_CHANNEL_NO           [JQKConfiguration sharedConfig].channelNo
 #define JQK_REST_APP_ID          @"QUBA_2004"
 #define JQK_REST_PV              @100
-#define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Jiangxi Electronic Group Corporation Ltd."
+#define JQK_PACKAGE_CERTIFICATE  @"iPhone Distribution: Tianjin Institute of Surveying and Mapping"
 
 #define JQK_WECHAT_APP_ID        @"wx4af04eb5b3dbfb56"
 #define JQK_WECHAT_MCH_ID        @"1281148901"
